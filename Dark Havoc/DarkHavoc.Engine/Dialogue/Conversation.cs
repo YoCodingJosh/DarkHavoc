984e8f01f917312999bf1bff84c320768551eb80
Josh Kennedy <allamericanjbert@gmail.com>
2014-01-02 06:45:00 -0600

    Started implementing a RPG-style dialogue engine.
    It's gonna be real cool man. :)

---
+++ Dark Havoc/DarkHavoc.Engine/Dialogue/Conversation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text;
+
+namespace DarkHavoc.Engine.Dialogue
+{
+    public static class Conversation
+    {
+        public static List<Speaker> ConversationSpeakers = new List<Speaker>();
+        private static int currentConversationSpeakerIndex = 0;
+        public static string ConversationFileLocation;
+    }
+}
